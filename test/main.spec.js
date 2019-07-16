c890a43e7e7970d21dbbddb8b9c890d780f370c2
Martín M <skydiver@users.noreply.github.com>
2019-07-16 01:38:41 -0300

    linting

--- test/main.spec.js
+++ test/main.spec.js
@@ -113,7 +113,7 @@ describe('env: serverless', () => {
 describe('valid credentials, invalid device', () => {
   test('get device power state should fail', async () => {
     const conn = new ewelink({ email, password });
-    const powerState = await conn.getDevicePowerState('invalid device id');
+    const powerState = await conn.getDevicePowerState('invalid deviceid');
     expect(typeof powerState).toBe('object');
     expect(powerState.msg).toBe('Device does not exist');
     expect(powerState.error).toBe(500);
@@ -122,7 +122,7 @@ describe('valid credentials, invalid device', () => {
   test('set device power state should fail', async () => {
     jest.setTimeout(30000);
     const conn = new ewelink({ email, password });
-    const powerState = await conn.setDevicePowerState('invalid device id ', 'on');
+    const powerState = await conn.setDevicePowerState('invalid deviceid', 'on');
     expect(typeof powerState).toBe('object');
     expect(powerState.msg).toBe('Device does not exist');
     expect(powerState.error).toBe(500);
