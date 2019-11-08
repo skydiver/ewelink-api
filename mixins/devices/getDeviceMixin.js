b0adde2d43e971069dd3c17d3af35fba1f2a3086
Martín M <skydiver@users.noreply.github.com>
2019-11-07 21:44:28 -0300

    handle error messages on new method

--- mixins/devices/getDeviceMixin.js
+++ mixins/devices/getDeviceMixin.js
@@ -1,3 +1,5 @@
+const { _get } = require('../../lib/helpers');
+
 const getDeviceMixin = {
   /**
    * Get information about all associated devices to account
@@ -7,8 +9,31 @@ const getDeviceMixin = {
    * @returns {Promise<{msg: string, error: *}>}
    */
   async getDevice(deviceId) {
+    // return this.makeRequest({
+    //   uri: `/user/device/${deviceId}`,
+    //   qs: { lang: 'en', getTags: 1 },
+    // });
+
     const devices = await this.getDevices();
-    return devices.devicelist.find(dev => dev.deviceid === deviceId);
+    const devicelist = _get(devices, 'devicelist', false);
+
+    const error = _get(devices, 'error', false);
+
+    if (error === 406) {
+      return { error: 401, msg: 'Authentication error' };
+    }
+
+    if (error || !devicelist) {
+      return devices;
+    }
+
+    const device = devicelist.find(dev => dev.deviceid === deviceId);
+
+    if (!device) {
+      return { error: 500, msg: 'Device does not exist' };
+    }
+
+    return device;
   },
 };
 
