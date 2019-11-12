2ebdb3a3915ba9e1dc2e7e4980dde71b90188952
Martin M <skydiver@users.noreply.github.com>
2019-11-12 10:13:44 -0300

    Release v1.8.2 (#30)
    * updated getDevices method
    
    * temporarily disable test
    
    * version bump

--- mixins/devices/getDevicesMixin.js
+++ mixins/devices/getDevicesMixin.js
@@ -1,14 +1,44 @@
+const { makeFakeIMEI } = require('../../lib/ewelink-helper');
+const { _get } = require('../../lib/helpers');
+
 const getDevicesMixin = {
   /**
-   * Get specific device information
+   * Get all devices information
    *
-   * @returns {Promise<{msg: string, error: *}>}
+   * @returns {Promise<{msg: string, error: number}|*>}
    */
   async getDevices() {
-    return this.makeRequest({
+    const timeStamp = new Date() / 1000;
+    const ts = Math.floor(timeStamp);
+
+    const response = await this.makeRequest({
       uri: '/user/device',
-      qs: { lang: 'en', getTags: 1 },
+      qs: {
+        lang: 'en',
+        getTags: 1,
+        version: 6,
+        ts,
+        appid: 'oeVkj2lYFGnJu5XUtWisfW4utiN4u9Mq',
+        imei: makeFakeIMEI(),
+        os: 'android',
+        model: '',
+        romVersion: '',
+        appVersion: '3.12.0',
+      },
     });
+
+    const error = _get(response, 'error', false);
+    const devicelist = _get(response, 'devicelist', false);
+
+    if (error === 406) {
+      return { error: 401, msg: 'Authentication error' };
+    }
+
+    if (!devicelist) {
+      return { error: 500, msg: 'No devices found' };
+    }
+
+    return devicelist;
   },
 };
 
