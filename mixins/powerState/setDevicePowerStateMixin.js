fa355ea77e01234936b47df44275867ac0c9aa1b
Marius Constantin <marius.constantin@superbet.ro>
2020-02-10 21:13:58 +0200

    Sonoff RF_Bridge transmitter added

--- mixins/powerState/setDevicePowerStateMixin.js
+++ mixins/powerState/setDevicePowerStateMixin.js
@@ -1,86 +1,108 @@
-const { _get } = require('../../lib/helpers');
-
-const { getDeviceChannelCount } = require('../../lib/ewelink-helper');
 const {
-  ChangeState,
-  ChangeStateZeroconf,
+    _get
+} = require('../../lib/helpers');
+
+const {
+    getDeviceChannelCount
+} = require('../../lib/ewelink-helper');
+const {
+    ChangeState,
+    ChangeStateZeroconf,
 } = require('../../classes/PowerState');
 
+const DEVICE_TYPE_UUID = require('../../lib/data/devices-type-uuid');
+
 const setDevicePowerState = {
-  /**
-   * Change power state for a specific device
-   *
-   * @param deviceId
-   * @param state
-   * @param channel
-   *
-   * @returns {Promise<{state: *, status: string}|{msg: string, error: *}>}
-   */
-  async setDevicePowerState(deviceId, state, channel = 1) {
-    const device = await this.getDevice(deviceId);
-    const deviceApiKey = _get(device, 'apikey', false);
-    const error = _get(device, 'error', false);
-    const uiid = _get(device, 'extra.extra.uiid', false);
+    /**
+     * Change power state for a specific device
+     *
+     * @param deviceId
+     * @param state
+     * @param channel
+     *
+     * @returns {Promise<{state: *, status: string}|{msg: string, error: *}>}
+     */
+    async setDevicePowerState(deviceId, state, channel = 1) {
+        const device = await this.getDevice(deviceId);
+        const deviceApiKey = _get(device, 'apikey', false);
+        const error = _get(device, 'error', false);
+        const uiid = _get(device, 'extra.extra.uiid', false);
 
-    let status = _get(device, 'params.switch', false);
-    const switches = _get(device, 'params.switches', false);
+        const params = {};
+        
+        switch(DEVICE_TYPE_UUID[uiid]){
+          case 'RF_BRIDGE':
+            // trigger button on RF_Bridge
+            params.setState = 'arm';
+            params.cmd = 'transmit';
+            params.rfChl = channel;
+            break;
+          default:
+            let status = _get(device, 'params.switch', false);
+            const switches = _get(device, 'params.switches', false);
 
-    const switchesAmount = getDeviceChannelCount(uiid);
+            const switchesAmount = getDeviceChannelCount(uiid);
 
-    if (switchesAmount > 0 && switchesAmount < channel) {
-      return { error, msg: 'Device channel does not exist' };
-    }
+            if (switchesAmount > 0 && switchesAmount < channel) {
+                return {
+                    error,
+                    msg: 'Device channel does not exist'
+                };
+            }
 
-    if (error || (!status && !switches)) {
-      if (error && parseInt(error) === 401) {
-        return device;
-      }
-      return { error, msg: 'Device does not exist' };
-    }
+            if (error || (!status && !switches)) {
+                if (error && parseInt(error) === 401) {
+                    return device;
+                }
+                return {
+                    error,
+                    msg: 'Device does not exist'
+                };
+            }
 
-    let stateToSwitch = state;
-    const params = {};
+            let stateToSwitch = state;
 
-    if (switches) {
-      status = switches[channel - 1].switch;
-    }
+            if (switches) {
+                status = switches[channel - 1].switch;
+            }
 
-    if (state === 'toggle') {
-      stateToSwitch = status === 'on' ? 'off' : 'on';
-    }
+            if (state === 'toggle') {
+                stateToSwitch = status === 'on' ? 'off' : 'on';
+            }
 
-    if (switches) {
-      params.switches = switches;
-      params.switches[channel - 1].switch = stateToSwitch;
-    } else {
-      params.switch = stateToSwitch;
-    }
+            if (switches) {
+                params.switches = switches;
+                params.switches[channel - 1].switch = stateToSwitch;
+            } else {
+                params.switch = stateToSwitch;
+            }
+        }
 
-    if (this.devicesCache) {
-      return ChangeStateZeroconf.set({
-        url: this.getZeroconfUrl(device),
-        device,
-        params,
-        switches,
-        state: stateToSwitch,
-      });
-    }
+        if (this.devicesCache) {
+            return ChangeStateZeroconf.set({
+                url: this.getZeroconfUrl(device),
+                device,
+                params,
+                switches,
+                state: typeof stateToSwitch != 'undefined' ? stateToSwitch : '',
+            });
+        }
 
-    const actionParams = {
-      apiUrl: this.getApiWebSocket(),
-      at: this.at,
-      apiKey: this.apiKey,
-      deviceId,
-      params,
-      state: stateToSwitch,
-    };
+        const actionParams = {
+            apiUrl: this.getApiWebSocket(),
+            at: this.at,
+            apiKey: this.apiKey,
+            deviceId,
+            params,
+            state: typeof stateToSwitch != 'undefined' ? stateToSwitch : '',
+        };
 
-    if (this.apiKey !== deviceApiKey) {
-      actionParams.apiKey = deviceApiKey;
-    }
+        if (this.apiKey !== deviceApiKey) {
+            actionParams.apiKey = deviceApiKey;
+        }
 
-    return ChangeState.set(actionParams);
-  },
+        return ChangeState.set(actionParams);
+    },
 };
 
 module.exports = setDevicePowerState;
