f4515f40f17c8806e76a245fcd0625da114af459
Martín M <skydiver@users.noreply.github.com>
2020-05-16 20:30:30 -0300

    refactor getDevicePowerState

--- src/mixins/getDevicePowerState.js
+++ src/mixins/getDevicePowerState.js
@@ -1,6 +1,7 @@
 const { _get } = require('../helpers/utilities');
 const errors = require('../data/errors');
-const { getDeviceChannelCount } = require('../helpers/ewelink');
+
+const deviceStatusPayload = require('../payloads/deviceStatus');
 
 module.exports = {
   /**
@@ -12,27 +13,30 @@ module.exports = {
    * @returns {Promise<{state: *, status: string}|{msg: string, error: *}>}
    */
   async getDevicePowerState(deviceId, channel = 1) {
-    const device = await this.getDevice(deviceId);
-    const error = _get(device, 'error', false);
-    const uiid = _get(device, 'extra.extra.uiid', false);
+    const status = await this.makeRequest({
+      uri: '/user/device/status',
+      qs: deviceStatusPayload({ deviceId, params: 'switch|switches' }),
+    });
 
-    let state = _get(device, 'params.switch', false);
-    const switches = _get(device, 'params.switches', false);
+    const error = _get(status, 'error', false);
 
-    const switchesAmount = getDeviceChannelCount(uiid);
+    if (error) {
+      return { error, msg: errors[error] };
+    }
+
+    let state = _get(status, 'params.switch', false);
+    const switches = _get(status, 'params.switches', false);
+
+    const switchesAmount = switches ? switches.length : 1;
 
     if (switchesAmount > 0 && switchesAmount < channel) {
       return { error: 404, msg: errors.ch404 };
-    }
-
-    if (error || (!state && !switches)) {
-      return { error, msg: errors[error] };
     }
 
     if (switches) {
       state = switches[channel - 1].switch;
     }
 
-    return { status: 'ok', state };
+    return { status: 'ok', state, channel };
   },
 };
