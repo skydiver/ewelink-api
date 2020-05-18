1af4c55ef31edb521575908bc42c2fae2e380f36
Martín M <skydiver@users.noreply.github.com>
2020-05-18 01:51:03 -0300

    refactor nock helper file

--- test/_setup/nock-helpers.js
+++ test/_setup/nock-helpers.js
@@ -18,35 +18,44 @@ exports.playbackNockTapes = pathToTape => {
   const nocks = nock.load(pathToTape);
 
   nocks.forEach(function(n) {
-    n.filteringPath(path => {
-      const regexTimestampInPath = /(?<=ts=)[^&]*/g;
-      const timestampInPath = path.match(regexTimestampInPath);
-
-      if (!timestampInPath) {
-        return path;
-      }
-
-      const regexTimestampInRecordedPath = /ts=[^&]*/g;
-      const recordedPath = n.interceptors[0].uri;
-      const timestampInRecordedPath = recordedPath.match(regexTimestampInPath);
-      const updatedPath = recordedPath.replace(
-        regexTimestampInRecordedPath,
-        `ts=${timestampInRecordedPath[0]}`
-      );
-
-      return updatedPath;
-    });
-
-    n.filteringRequestBody((body, recordedBody) => {
-      if (typeof body !== 'string' || typeof recordedBody !== 'object') {
-        return body;
-      }
-      const jsonBody = JSON.parse(body);
-      jsonBody.ts = recordedBody.ts;
-      jsonBody.nonce = recordedBody.nonce;
-      return JSON.stringify(jsonBody);
-    });
+    n.filteringPath(path => filteringPath(n, path));
+    n.filteringRequestBody((b, rb) => filteringRequestBody(b, rb));
   });
 
   nock.recorder.play();
+};
+
+/**
+ * Replace dynamic values on request body using recorded data
+ */
+const filteringRequestBody = (body, recordedBody) => {
+  if (typeof body !== 'string' || typeof recordedBody !== 'object') {
+    return body;
+  }
+  const jsonBody = JSON.parse(body);
+  jsonBody.ts = recordedBody.ts;
+  jsonBody.nonce = recordedBody.nonce;
+  return JSON.stringify(jsonBody);
+};
+
+/**
+ * Replace dynamic values on request URLs using recorded data
+ */
+const filteringPath = (n, path) => {
+  const regexTimestampInPath = /(?<=ts=)[^&]*/g;
+  const timestampInPath = path.match(regexTimestampInPath);
+
+  if (!timestampInPath) {
+    return path;
+  }
+
+  const regexTimestampInRecordedPath = /ts=[^&]*/g;
+  const recordedPath = n.interceptors[0].uri;
+  const timestampInRecordedPath = recordedPath.match(regexTimestampInPath);
+  const updatedPath = recordedPath.replace(
+    regexTimestampInRecordedPath,
+    `ts=${timestampInRecordedPath[0]}`
+  );
+
+  return updatedPath;
 };
