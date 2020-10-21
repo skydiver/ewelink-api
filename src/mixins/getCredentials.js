012d0ee092d318f46449cf2210496a7630339b8f
Martín M <skydiver@users.noreply.github.com>
2020-10-21 19:15:22 -0300

    getCredentials: use v2 API

--- src/mixins/getCredentials.js
+++ src/mixins/getCredentials.js
@@ -1,50 +1,41 @@
 const fetch = require('node-fetch');
 
 const { _get } = require('../helpers/utilities');
-const credentialsPayload = require('../payloads/credentialsPayload');
 const { makeAuthorizationSign } = require('../helpers/ewelink');
-const errors = require('../data/errors');
 
 module.exports = {
   /**
    * Returns user credentials information
-   *
-   * @returns {Promise<{msg: string, error: *}>}
    */
   async getCredentials() {
     const { APP_ID, APP_SECRET } = this;
 
-    const body = credentialsPayload({
-      appid: APP_ID,
+    const body = {
+      countryCode: '+1',
       email: this.email,
-      phoneNumber: this.phoneNumber,
       password: this.password,
-    });
+    };
 
-    const request = await fetch(`${this.getApiUrl()}/user/login`, {
+    if (this.phoneNumber) {
+      body.phoneNumber = this.phoneNumber;
+    }
+
+    const request = await fetch(`${this.getApiUrl()}/v2/user/login`, {
       method: 'post',
       headers: {
         Authorization: `Sign ${makeAuthorizationSign(APP_SECRET, body)}`,
+        'Content-Type': 'application/json',
+        'X-CK-Appid': APP_ID,
       },
       body: JSON.stringify(body),
     });
 
-    let response = await request.json();
+    const response = await request.json();
 
     const error = _get(response, 'error', false);
-    const region = _get(response, 'region', false);
 
-    if (error && [400, 401, 404].indexOf(parseInt(error)) !== -1) {
-      return { error: 406, msg: errors['406'] };
-    }
-
-    if (error && parseInt(error) === 301 && region) {
-      if (this.region !== region) {
-        this.region = region;
-        response = await this.getCredentials();
-        return response;
-      }
-      return { error, msg: 'Region does not exist' };
+    if (error) {
+      throw new Error(`Error ${error}: ${response.msg}`);
     }
 
     this.apiKey = _get(response, 'user.apikey', '');
