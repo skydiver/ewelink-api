4038c8c7bda828913f331dd6da27ae1f4fc9a6ad
Martín M <skydiver@users.noreply.github.com>
2020-10-22 02:18:53 -0300

    checkDeviceUpdate: use v2 API

--- src/mixins/checkDeviceUpdate.js
+++ src/mixins/checkDeviceUpdate.js
@@ -11,38 +11,25 @@ module.exports = {
    */
   async checkDeviceUpdate(deviceId) {
     const device = await this.getDevice(deviceId);
-
-    const error = _get(device, 'error', false);
-
-    if (error) {
-      return device;
-    }
-
     const deviceInfoList = parseFirmwareUpdates([device]);
-
-    const deviceInfoListError = _get(deviceInfoList, 'error', false);
-
-    if (deviceInfoListError) {
-      return deviceInfoList;
-    }
 
     const update = await this.makeRequest({
       method: 'post',
-      url: this.getOtaUrl(),
-      uri: '/app',
+      uri: '/v2/device/ota/query',
       body: { deviceInfoList },
     });
 
-    const isUpdate = _get(update, 'upgradeInfoList.0.version', false);
+    const isUpdate = _get(update, 'otaInfoList.0.version', false);
 
-    if (!isUpdate) {
-      return { status: 'ok', msg: 'No update available' };
-    }
-
-    return {
-      status: 'ok',
-      msg: 'Update available',
-      version: isUpdate,
-    };
+    return isUpdate
+      ? {
+          status: 'ok',
+          msg: 'Update available',
+          version: isUpdate,
+        }
+      : {
+          status: 'ok',
+          msg: 'No update available',
+        };
   },
 };
