57e4caf3f10c23efd73c408312e2bf9d92493f25
paulomar <paulomartins@gmail.com>
2021-02-01 22:19:42 -0300

    Fix socket close timeout

--- src/mixins/openWebSocket.js
+++ src/mixins/openWebSocket.js
@@ -1,3 +1,5 @@
+// Fix from: https://github.com/skydiver/ewelink-api/issues/65
+
 const W3CWebSocket = require('websocket').w3cwebsocket;
 const WebSocketAsPromised = require('websocket-as-promised');
 
@@ -24,21 +26,39 @@ module.exports = {
     });
 
     wsp.onMessage.addListener(message => {
-      try {
-        const data = JSON.parse(message);
-        callback(data);
-      } catch (error) {
-        callback(message);
+      if (message === 'pong') {
+        // eat pong...
+        callback({ action: 'socket', message: 'pong' });
+      } else {
+        try {
+          const data = JSON.parse(message);
+          callback(data);
+        } catch (error) {
+          callback(message);
+        }
       }
     });
 
+    wsp.onOpen.addListener(() => {
+      callback({ action: 'socket', message: 'opened' });
+    });
+    wsp.onClose.addListener(() => {
+      callback({ action: 'socket', message: 'closed' });
+    });
+    wsp.onError.addListener(() => {
+      callback({ action: 'socket', message: 'error' });
+    });
     await wsp.open();
     await wsp.send(payloadLogin);
 
     setInterval(async () => {
-      await wsp.send('ping');
+      if (wsp.isClosed || wsp.isClosing) {
+        console.log(new Date().toISOString(), 'wsp.isClosed || wsp.isClosing');
+        callback({ action: 'socket', message: 'closed' });
+      }
+      console.log(new Date().toISOString(), 'ping sent...');
+      wsp.send('ping'); // await
     }, heartbeat);
-
     return wsp;
   },
 };
